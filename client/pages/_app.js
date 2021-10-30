a652d35f761dafdbc75aa223f6f4837ebfa2a18f
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-10-30 17:55:17 +0300

    init

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import '/css/style.css'; 
+import '/css/bootstrap.min.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
